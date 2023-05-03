e88e99d8bf20f32dae9e5ee0cac2b83990612d97
Olivier Dupuis <odupuis@lantrns.co>
2023-05-03 13:01:38 -0400

    Changing how api key is passed to openapi calls

--- saf_openai/openai_resource.py
+++ saf_openai/openai_resource.py
@@ -3,10 +3,11 @@ import openai
 
 class OpenAIResource:
     def __init__(self, openai_api_key):
-        openai.api_key = openai_api_key
+        self._openai_api_key = openai_api_key
     
 
     def completion(self, model, prompt, max_tokens=16, temperature=1):
+        openai.api_key = self._openai_api_key
         # Generate text using the OpenAI API
         response = openai.Completion.create(
             model=model,
